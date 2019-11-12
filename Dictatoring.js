19e59e8315e429e6a9935e24b239be303d73a3a4
Rotartsi82 <grantthewarhero@gmail.com>
2019-11-11 17:04:34 -0800

    Updated variables to suit US & added "Years in power" counter

--- Dictatoring.js
+++ Dictatoring.js
@@ -1,35 +1,15 @@
+let log = function (X) {
+    console.log(X);
+};
+
 function clearCookie() {
     document.cookie = "name=";
     document.cookie = "place=";
 }
 
-// clearCookie();
-var unshrinkify = function (x) {
-    x = x + "";
-    var c = x.charAt(x.length - 1);
-    if (c === "T") {
-        return parseFloat(x.substring(0, x.length - 1))*1000000000000;
-    } else if (c === "B") {
-        return parseFloat(x.substring(0, x.length - 1))*1000000000;
-    } else if (c === "M") {
-        return parseFloat(x.substring(0, x.length - 1))*1000000;
-    } else if (c === "K") {
-        return parseFloat(x.substring(0, x.length - 1))*1000;
-    }
-    return parseFloat(x);
-};
-var shrinkify = function (x) {
-    if (Math.abs(x) / 1000000000000 >= 1) {
-        return Math.round(x/1000000000)/1000 + "T";
-    } else if (Math.abs(x) / 1000000000 >= 1) {
-        return Math.round(x/1000000)/1000 + "B";
-    } else if (Math.abs(x) / 1000000 >= 1) {
-        return Math.round(x/1000)/1000 + "M";
-    } else if (Math.abs(x) / 1000 >= 1) {
-        return Math.round(x)/1000 + "K";
-    }
-    return x;
-};
+function rand(min, max) {
+    return Math.random() * (max - min + 1) + min;
+}
 
 function getCookie(cname) {
     var name = cname + "=";
@@ -46,27 +26,61 @@ function getCookie(cname) {
     }
     return "";
 }
-var birthrate = 0.0189;
+
+// clearCookie();
+
+var unshrinkify = function (x) {
+    x = x + "";
+    var c = x.charAt(x.length - 1);
+    if (c === "T") {
+        return parseFloat(x.substring(0, x.length - 1))*1000000000000;
+    } else if (c === "B") {
+        return parseFloat(x.substring(0, x.length - 1))*1000000000;
+    } else if (c === "M") {
+        return parseFloat(x.substring(0, x.length - 1))*1000000;
+    } else if (c === "K") {
+        return parseFloat(x.substring(0, x.length - 1))*1000;
+    }
+    return parseFloat(x);
+};
+
+var shrinkify = function (x) {
+    if (Math.abs(x) / 1000000000000 >= 1) {
+        return Math.round(x/1000000000)/1000 + "T";
+    } else if (Math.abs(x) / 1000000000 >= 1) {
+        return Math.round(x/1000000)/1000 + "B";
+    } else if (Math.abs(x) / 1000000 >= 1) {
+        return Math.round(x/1000)/1000 + "M";
+    } else if (Math.abs(x) / 1000 >= 1) {
+        return Math.round(x)/1000 + "K";
+    }
+    return x;
+};
+
+var birthrate = 0.00760;
 var name, place;
 var ask = document.getElementById("ask");
 var question = document.getElementById("question");
+var yearcounter = document.getElementById("year-counter");
 var answer = document.getElementById("answer");
 var submit = document.getElementById("submit");
 var navbar = document.getElementById("navbar");
 var buyMenu = document.getElementById("buymenu");
 var dictatorMenu = document.getElementById("dictatorMenu");
-var money = 1000000000;
+var money = 3000000000000;
 var moneyText = document.getElementById("money");
-var population = 100;
+var population = 329968629;
 var populationText = document.getElementById("population");
 var taxValue = document.getElementById("taxValue");
 var taxSlider = document.getElementById("taxes");
 var taxes = 0;
+
 taxes = taxSlider.value;
 taxSlider.oninput = function() {
     taxes = this.value;
     taxValue.textContent = taxes;
 };
+
 var pps = 0;
 var mps = 0;
 var menus = document.getElementsByClassName("menu");
@@ -75,9 +89,10 @@ var powerPerDay = 0;
 var powerPerDayText = document.getElementById("powerperday");
 var waterPerDay = 0;
 var waterPerDayText = document.getElementById("waterperday");
-var averageWages = 120;
-var averageMoney = 500000;
-console.log(document.cookie);
+var averageWages = 68815.9090909091;
+var averageMoney = 114457.142857143;  // Is this the average money owned by each person?
+log(document.cookie);
+
 answer.addEventListener("keyup", function(event) {
     if (event.key.toLowerCase() === "enter") {
         event.preventDefault();
@@ -89,40 +104,43 @@ function askName() {
     ask.classList.remove("hidden");
     question.textContent = "What is your name?";
     submit.onclick = gotName;
-    console.log("ask name");
+    log("ask name");
 }
 
 function gotName() {
     name = answer.value;
     answer.value = '';
     document.cookie = "name=" + name;
-    console.log(name);
+    log(name);
     ask.classList.add("hidden");
     if (getCookie("place").length === 0) {
         askPlace()
     } else {
         place = getCookie("place");
-        console.log(place);
+        log(place);
         startGame();
     }
-    console.log("got name");
+    log("got name");
 }
+
 function askPlace() {
     ask.classList.remove("hidden");
     question.textContent = "What is the name of your country?";
     submit.onclick = gotPlace;
-    console.log("ask place");
+    log("ask place");
 }
+
 function gotPlace() {
     place = answer.value;
     answer.value = '';
     document.cookie = "place=" + place;
-    console.log(place);
+    log(place);
     ask.classList.add("hidden");
-    console.log("got place");
-    console.log(document.cookie);
+    log("got place");
+    log(document.cookie);
     startGame();
 }
+
 function startGame() {
     taxSlider.value = 0;
     navbar.classList.remove("hidden");
@@ -134,9 +152,9 @@ function startGame() {
 for (let i = 0; i < menus.length; i++) {
     menus[i].addEventListener('click', function () {
         document.getElementById(currmenu.textContent + "Buy").classList.add("hidden");
-        console.log(currmenu);
+        log(currmenu);
         currmenu = this;
-        console.log(currmenu);
+        log(currmenu);
         document.getElementById(currmenu.textContent + "Buy").classList.remove("hidden");
     })
 }
@@ -144,33 +162,35 @@ if (getCookie("name").length === 0) {
     askName();
 } else {
     name = getCookie("name");
-    console.log(name);
+    log(name);
     if (getCookie("place").length === 0) {
         askPlace();
     } else {
         place = getCookie("place");
-        console.log(place);
+        log(place);
         startGame();
     }
 }
-function update() {
+
+var years = 0;
+
+function update() {  // Assuming that each update is a year
     averageMoney += averageWages;
-    let change = averageMoney*taxes;
-    mps = Math.floor(change*population);
+    let change = averageMoney * taxes;
+
+    mps = Math.floor(change * population);
     averageMoney -= change;
-    averageMoney = Math.max(0, averageMoney);2
-    pps = 0;
-    for (let i = 0; i < population; i++) {
-        let x = Math.random();
-        if (x <= birthrate) {
-            console.log(x);
-            pps++;
-        }
-    }
+    averageMoney = Math.max(0, averageMoney);
+
+    pps = Math.floor(population * birthrate * rand(0.875, 1.125));
+
     money += mps;
     population += pps;
     moneyText.textContent = shrinkify(money);
     populationText.textContent = shrinkify(population);
     powerPerDayText.textContent = shrinkify(powerPerDay);
     waterPerDayText.textContent = shrinkify(waterPerDay);
+
+    years++;
+    yearcounter.textContent = years;
 }
